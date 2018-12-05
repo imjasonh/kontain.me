1e6a14d42771b8bbfdf34c3fa5a2c47ba3d20087
Jason Hall <jasonhall@google.com>
2018-12-05 11:06:10 -0500

    Write to and serve from GCS, fix some bugs

--- main.go
+++ main.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"os/exec"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/google/go-containerregistry/pkg/ko/build"
+	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1"
-	"github.com/google/go-containerregistry/pkg/v1/empty"
 	"github.com/google/go-containerregistry/pkg/v1/random"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
 )
 
-const (
-	gcs    = false
-	bucket = "konta.in"
-)
+const bucket = "kontainme"
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -46,71 +45,58 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	case strings.HasPrefix(path, "random/manifests/"):
 		serveRandomManifest(w, r)
-	case strings.HasPrefix(path, "ko/manifests/"):
+	case strings.HasPrefix(path, "ko/") && strings.Contains(path, "/manifests/"):
 		serveKoManifest(w, r)
 	case strings.HasPrefix(path, "random/blobs/"),
 		strings.HasPrefix(path, "ko/blobs/"):
 		serveBlob(w, r)
+	default:
+		http.Error(w, "not found", http.StatusNotFound)
 	}
 }
 
-// TODO: serve blobs by redirecting to GCS.
 func serveBlob(w http.ResponseWriter, r *http.Request) {
-	// Extract requested blob digest and serve it.
+	// Extract requested blob digest and redirect to serve it from GCS.
 	// If it doesn't exist, this will return 404.
 	parts := strings.Split(r.URL.Path, "/")
 	digest := parts[len(parts)-1]
-	if gcs {
-		url := fmt.Sprintf("https://storage.googleapis.com/%s/blobs/%s", bucket, digest)
-		http.Redirect(w, r, url, http.StatusSeeOther)
-	} else {
-		http.ServeFile(w, r, "blobs/"+digest)
-	}
+	url := fmt.Sprintf("https://storage.googleapis.com/%s/blobs/%s", bucket, digest)
+	http.Redirect(w, r, url, http.StatusSeeOther)
 }
 
-// TODO: write blobs to GCS.
 func writeBlob(h v1.Hash, rc io.ReadCloser) error {
-	if gcs {
-		// TODO:
-	} else {
-		path := "blobs/" + h.String()
-		// Check if file exists already.
-		if _, err := os.Stat(path); err == nil {
-			log.Printf("%q already exists", h)
-			return nil
-		}
-
-		f, err := os.Create(path)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		if _, err := io.Copy(f, rc); err != nil {
-			return err
-		}
-		if err := rc.Close(); err != nil {
-			return err
-		}
+	ctx := context.Background() // TODO
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("NewClient: %v", err)
+	}
+	w := client.Bucket(bucket).Object(fmt.Sprintf("blobs/%s", h)).If(storage.Conditions{DoesNotExist: true}).NewWriter(ctx)
+	if _, err := io.Copy(w, rc); err != nil {
+		return fmt.Errorf("Copy: %v", err)
+	}
+	if err := rc.Close(); err != nil {
+		return fmt.Errorf("rc.Close: %v", err)
+	}
+	if err := w.Close(); err != nil {
+		return fmt.Errorf("w.Close: %v", err)
 	}
 	return nil
 }
 
 func getDefaultBaseImage(string) (v1.Image, error) {
-	return empty.Image, nil
-	/*
-		ref, err := name.ParseReference("gcr.io/distroless/base", name.WeakValidation)
-		if err != nil {
-			return nil, err
-		}
-		return remote.Image(ref)
-	*/
+	// TODO: memoize
+	ref, err := name.ParseReference("gcr.io/distroless/base", name.WeakValidation)
+	if err != nil {
+		return nil, err
+	}
+	return remote.Image(ref)
 }
 
 // konta.in/ko/github.com/knative/build/cmd/controller -> ko build and serve
 func serveKoManifest(w http.ResponseWriter, r *http.Request) {
-	path := strings.TrimPrefix(r.URL.Path, "/v2/ko/manifests/")
+	path := strings.TrimPrefix(r.URL.Path, "/v2/ko/")
 	parts := strings.Split(path, "/")
-	pkg := strings.Join(parts[:len(parts)-1], "/")
+	pkg := strings.Join(parts[:len(parts)-2], "/")
 
 	tag := parts[len(parts)-1]
 	log.Printf("requested image tag :%s", tag)
@@ -118,7 +104,7 @@ func serveKoManifest(w http.ResponseWriter, r *http.Request) {
 	// go get the package.
 	log.Printf("go get %s...", pkg)
 	if err := exec.Command("go", "get", pkg).Run(); err != nil {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (go get): %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -132,18 +118,18 @@ func serveKoManifest(w http.ResponseWriter, r *http.Request) {
 		build.WithCreationTime(v1.Time{time.Unix(0, 0)}),
 	)
 	if err != nil {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (build.NewGo): %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	if !g.IsSupportedReference(pkg) {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (IsSupportedReference): %s", err)
 		http.Error(w, fmt.Sprintf("%q is not a supported reference", pkg), http.StatusBadRequest)
 		return
 	}
 	img, err := g.Build(pkg)
 	if err != nil {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (ko build): %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -170,7 +156,7 @@ func serveRandomManifest(w http.ResponseWriter, r *http.Request) {
 	// Generate a random image.
 	img, err := random.Image(size, num)
 	if err != nil {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (random.Image): %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -183,19 +169,19 @@ func serveManifest(w http.ResponseWriter, img v1.Image) {
 	// Write config blob for later serving.
 	ch, err := img.ConfigName()
 	if err != nil {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (serveManifest ConfigName): %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	cb, err := img.RawConfigFile()
 	if err != nil {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (serveManifest RawConfigFile): %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	log.Printf("writing config blob %q", ch)
 	if err := writeBlob(ch, ioutil.NopCloser(bytes.NewReader(cb))); err != nil {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (serveManifest writeBlob): %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -203,26 +189,26 @@ func serveManifest(w http.ResponseWriter, img v1.Image) {
 	// Write layer blobs for later serving.
 	layers, err := img.Layers()
 	if err != nil {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (serveManifest Layers): %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	for _, l := range layers {
 		rc, err := l.Compressed()
 		if err != nil {
-			log.Println("ERROR: %s", err)
+			log.Printf("ERROR (serveManifest l.Compressed): %s", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		lh, err := l.Digest()
 		if err != nil {
-			log.Println("ERROR: %s", err)
+			log.Printf("ERROR (serveManifest l.Digest): %s", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		log.Printf("writing layer blob %q", lh)
 		if err := writeBlob(lh, rc); err != nil {
-			log.Println("ERROR: %s", err)
+			log.Printf("ERROR (serveManifest writeBlob): %s", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -231,9 +217,20 @@ func serveManifest(w http.ResponseWriter, img v1.Image) {
 	// Serve the manifest.
 	b, err := img.RawManifest()
 	if err != nil {
-		log.Println("ERROR: %s", err)
+		log.Printf("ERROR (serveManifest RawManifest): %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Write(b)
+	mt, err := img.MediaType()
+	if err != nil {
+		log.Printf("ERROR (serveManifest MediaType): %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", string(mt))
+	if _, err := io.Copy(w, bytes.NewReader(b)); err != nil {
+		log.Printf("ERROR (serveManifest Copy): %s", err)
+		return
+	}
+	fmt.Printf("Served manifest: %s", string(b))
 }
