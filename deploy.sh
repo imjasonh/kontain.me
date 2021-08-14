12fdb61463cbd1b556d902254089359ef33c30cf
Jason Hall <jasonhall@redhat.com>
2021-08-14 14:40:17 -0400

    remove terraform

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,4 @@
 
 set -euxo pipefail
 
-export KO_DOCKER_REPO=gcr.io/kontaindotme
-out=$(mktemp -d)/tmp.tfvars
-
-echo writing to $out
-
-echo "images = {" > $out
-cd cmd
-export KO_CONFIG_PATH=../
-for d in *;  do
-	echo "  \"$d\" = \"$(ko publish -P ./$d)\"" >> $out
-done
-echo "}" >> $out
-
-cat $out
-
-cd -
-
-terraform apply -var-file=$out
+time find cmd -name "deploy.sh" -exec {} \;
