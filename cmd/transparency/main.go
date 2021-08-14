1f766ba9d2e740ea9391b51102d51f939e65cb9f
Jason Hall <jasonhall@redhat.com>
2021-08-14 16:25:22 -0400

    crib from Jon's cmd/mirror

--- cmd/transparency/main.go
+++ cmd/transparency/main.go
@@ -12,9 +12,11 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
 	"github.com/imjasonh/kontain.me/pkg/serve"
 )
 
@@ -44,115 +46,132 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimPrefix(r.URL.String(), "/v2/")
 	parts := strings.Split(path, "/")
 
+	if r.Method != http.MethodGet && r.Method != http.MethodHead {
+		serve.Error(w, serve.ErrNotFound)
+		return
+	}
+
 	switch {
 	case r.URL.Path == "/v2/":
 		// API Version check.
 		w.Header().Set("Docker-Distribution-API-Version", "registry/2.0")
 		return
 	case parts[len(parts)-2] == "blobs":
-		s.redirect(w, r)
+		s.handleBlobs(w, r)
 	case parts[len(parts)-2] == "manifests":
-		s.checkManifest(w, r)
+		s.handleManifests(w, r)
 	default:
 		serve.Error(w, serve.ErrNotFound)
 	}
 }
 
-// path /v2/gcr.io/foo/bar/baz/blobs/sha -> https://gcr.io/v2/foo/bar/baz/blobs/sha
-// path /v2/ubuntu/blobs/sha -> https://index.docker.io/v2/library/ubuntu/blobs/sha
-func (s *server) redirect(w http.ResponseWriter, r *http.Request) {
+func reqToRef(r *http.Request) (name.Reference, error) {
 	parts := strings.Split(r.URL.Path, "/")
-	if len(parts) < 5 {
+	parts = parts[1:]
+	target := parts[len(parts)-1]
+	repo := strings.Join(parts[1:len(parts)-2], "/")
+
+	if strings.Contains(target, ":") {
+		return name.ParseReference(repo + "@" + target)
+	}
+	return name.ParseReference(repo + ":" + target)
+}
+
+func (s *server) handleBlobs(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	parts = parts[1:]
+	if parts[len(parts)-1] == "" {
+		parts = parts[:len(parts)-1]
+	}
+	// Must have a path of form /v2/{name}/blobs/sha256:...
+	if len(parts) < 4 {
 		serve.Error(w, serve.ErrNotFound)
 		return
 	}
-	repo := strings.Join(parts[2:len(parts)-2], "/")
-	ref, err := name.ParseReference(repo)
+
+	ref, err := reqToRef(r)
 	if err != nil {
-		s.error.Printf("ERROR (ParseReference(%q)): %v", repo, err)
+		serve.Error(w, err)
+		return
+	}
+
+	tr, err := transport.NewWithContext(r.Context(), ref.Context().Registry, authn.Anonymous, http.DefaultTransport, []string{ref.Scope(transport.PullScope)})
+	if err != nil {
+		s.error.Println(err)
 		serve.Error(w, err)
 		return
 	}
 
 	dig := parts[len(parts)-1]
 	url := "https://" + ref.Context().RegistryStr() + "/v2/" + ref.Context().String() + "/blobs/" + dig
-	s.info.Println("redirecting to", url)
-	http.Redirect(w, r, url, http.StatusSeeOther)
+	s.info.Println("url:", url)
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		s.error.Println(err)
+		serve.Error(w, err)
+		return
+	}
+	res, err := tr.RoundTrip(req)
+	if err != nil {
+		s.error.Println(err)
+		serve.Error(w, err)
+		return
+	}
+	w.Header().Set("Location", res.Header.Get("Location"))
+	w.WriteHeader(res.StatusCode)
 }
 
-// /v2/gcr.io/foo/bar/baz/manifests/tag-name -> gcr.io/foo/bar/baz:tag-name
-func (s *server) checkManifest(w http.ResponseWriter, r *http.Request) {
+func (s *server) handleManifests(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	parts := strings.Split(r.URL.Path, "/")
-	if len(parts) < 4 {
-		serve.Error(w, serve.ErrNotFound)
-		return
-	}
-	// Parse and canonicalize to handle "ubuntu" -> "index.docker.io/library/ubuntu"
-	last := parts[len(parts)-1]
-	sep := ":"
-	if strings.Contains(last, ":") {
-		sep = "@"
-	}
-	refstr := strings.Join(parts[2:len(parts)-2], "/") + sep + last
-	s.info.Println("request for ref", refstr)
-	ref, err := name.ParseReference(refstr)
+	ref, err := reqToRef(r)
 	if err != nil {
-		s.error.Printf("ERROR (ParseReference(%q)): %v", refstr, err)
+		s.error.Println(err)
 		serve.Error(w, err)
 		return
 	}
 
-	// Look up the ref's current digest.
-	desc, err := remote.Get(ref, remote.WithContext(ctx))
+	desc, err := remote.Get(ref)
 	if err != nil {
-		s.error.Printf("ERROR (remote.Get(%q)): %v", ref, err)
+		s.error.Println(err)
 		serve.Error(w, err)
 		return
 	}
-	cur := desc.Digest
-	s.info.Printf("current: %s -> %s", ref, cur)
 
-	// If request is by digest, serve it directly.
-	if _, ok := ref.(name.Digest); ok {
-		w.Header().Set("Docker-Content-Digest", desc.Digest.String())
-		w.Header().Set("Content-Type", string(desc.MediaType))
-		w.Header().Set("Content-Length", strconv.Itoa(int(desc.Size)))
-		w.WriteHeader(http.StatusOK)
-		io.Copy(w, bytes.NewReader(desc.Manifest))
-		return
-	}
-
-	// Look up ref in rekor log.
-	got, err := s.lookup(ctx, ref)
-	if err == errRekordNotFound {
-		// Ref wasn't found, record it.
-		if err := s.record(ctx, ref, cur); err != nil {
-			s.error.Printf("ERROR (record): %v", ref, err)
+	if _, ok := ref.(name.Digest); !ok {
+		// Request isn't by digest; let's check it!
+		// Look up ref in rekor log.
+		cur := desc.Digest
+		got, err := s.lookup(ctx, ref)
+		if err == errRekordNotFound {
+			// Ref wasn't found, record it.
+			if err := s.record(ctx, ref, cur); err != nil {
+				s.error.Printf("ERROR (record): %v", ref, err)
+				serve.Error(w, err)
+				return
+			}
+		} else if err != nil {
+			// Lookup failed!
+			s.error.Printf("ERROR (lookup): %v", ref, err)
 			serve.Error(w, err)
 			return
+		} else {
+			s.info.Printf("rekor: %s -> %s", ref, got)
+			if got != cur {
+				s.error.Printf("ERROR (mismatch): got %q, want %q", got, cur)
+				serve.Error(w, fmt.Errorf("rekor digest mismatch: got %q, want %q", got, cur))
+				return
+			}
+			// Otherwise, proceed and redirect.
 		}
-	} else if err != nil {
-		// Lookup failed!
-		s.error.Printf("ERROR (lookup): %v", ref, err)
-		serve.Error(w, err)
-		return
-	} else {
-		s.info.Printf("rekor: %s -> %s", ref, got)
-		if got != cur {
-			s.error.Printf("ERROR (mismatch): got %q, want %q", got, cur)
-			serve.Error(w, fmt.Errorf("rekor digest mismatch: got %q, want %q", got, cur))
-			return
-		}
-		// Otherwise, proceed and redirect.
 	}
 
-	// Write the manifest out.
 	w.Header().Set("Docker-Content-Digest", desc.Digest.String())
 	w.Header().Set("Content-Type", string(desc.MediaType))
 	w.Header().Set("Content-Length", strconv.Itoa(int(desc.Size)))
 	w.WriteHeader(http.StatusOK)
-	io.Copy(w, bytes.NewReader(desc.Manifest))
+	if r.Method == http.MethodGet {
+		io.Copy(w, bytes.NewReader(desc.Manifest))
+	}
 }
 
 var errRekordNotFound = errors.New("record not found in rekor")
---
+++ cmd/transparency/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+time crane validate --remote=transparency.kontain.me/ubuntu
+
+# TODO: push to an ephemeral tag, check it, push to it again, should fail, cleanup tag
