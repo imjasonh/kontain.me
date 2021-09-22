a92de278260ed22e0c0539d503649a752674f828
Jason Hall <jasonhall@redhat.com>
2021-09-22 11:51:43 -0400

    test after deployment

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 set -euxo pipefail
 
-
 find cmd -name 'deploy.sh' -print0 | 
     while IFS= read -r -d '' line; do 
         bash -c "$line"
--- test.sh
+++ test.sh
@@ -2,4 +2,7 @@
 
 set -euxo pipefail
 
-time find cmd -name "test.sh" -exec {} \;
+find cmd -n 'test.sh' -print0 |
+    while IFS= read -r -d '' line; do 
+        bash -c "$line"
+    done
