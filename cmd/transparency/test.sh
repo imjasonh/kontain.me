f1f11f46add82ef7ee36f67132c1a61103ecfecd
Jason Hall <jasonhall@redhat.com>
2021-09-22 12:57:15 -0400

    disable transparency test

--- cmd/transparency/test.sh
+++ cmd/transparency/test.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-time crane validate --remote=transparency.kontain.me/ubuntu
+#time crane validate --remote=transparency.kontain.me/ubuntu
 
 # TODO: push to an ephemeral tag, check it, push to it again, should fail, cleanup tag
